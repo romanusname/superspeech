ed20e5d62a6c0b169e9a9dcb0106192e33123277
romanusname <romanusname@gmail.com>
2024-11-21 23:00:14 +0700

    new

--- src/app/page.js
+++ src/app/page.js
@@ -12,13 +12,18 @@ export default function Home() {
     window.location.href = 'https://go.2gis.com/h0eda';
   };
 
+  const handleClick3 = () => {
+    window.location.href = 'https://prodoctorov.ru/novosibirsk/vrach/202476-ivanova/';
+  };
+
   //https://go.2gis.com/buvi8
 
   return (
     <div className={"wrapper"}>
-    <h1>Где хотите оставить отзыв?</h1>
+    <h1>Буду благодарна за оставленный отзыв 😊</h1>
       <button onClick={handleClick1}>Яндекс</button>
       <button onClick={handleClick2}>2GIS</button>
+      <button onClick={handleClick3}>ПроДокторов</button>
     </div>
   );
 }
